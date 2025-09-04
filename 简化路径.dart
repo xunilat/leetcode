61ba15cd3b1504f2da94ae5eb7965c3fa2c29e95
xunilat <xunilat@gmail.com>
2025-09-04 21:56:24 +0800

    leetcode 71 optimization

--- 简化路径.dart
+++ 简化路径.dart
@@ -1,47 +1,28 @@
 class Solution {
   String simplifyPath(String path) {
-    List<String> res = ['/'];
-    for (int i = 0; i < path.length; i++) {
-      if (path[i] == '/') {
-        if (res.last == '/.') {
-          res.removeLast();
-          if (res.isEmpty) res.add('/');
-        }
-        if (res.last == '/..') {
-          res.removeLast();
-          if (res.isEmpty) res.add('/');
-          res.removeLast();
-          if (res.isEmpty) res.add('/');
-        }
-
-        if (res.last != '/') {
-          res.add('/');
-        }
+    List<String> res = [];
+    for (int i = 0; i <= path.length; i++) {
+      if (res.isEmpty) res.add('');
+      if (i == path.length || path[i] == '/') {
+        if (res.last == '.') res.removeLast();
+        if (res.isNotEmpty && res.last == '..')
+          res.length > 2
+              ? res.removeRange(res.length - 2, res.length)
+              : res.clear();
+        if (res.isNotEmpty && res.last != '') res.add('');
         continue;
       }
-
       res.last += path[i];
     }
+    if (res.isNotEmpty) res.removeLast();
 
-    if (res.last == '/.') {
-      res.removeLast();
-      if (res.isEmpty) res.add('/');
-    }
-    if (res.last == '/..') {
-      res.removeLast();
-      if (res.isEmpty) res.add('/');
-      res.removeLast();
-      if (res.isEmpty) res.add('/');
-    }
-    if (res.length > 1 && res.last == '/') res.removeLast();
-
-    return res.join();
+    return '/' + res.join('/');
   }
 }
 
 void main() {
   Solution solution = Solution();
-  print(solution.simplifyPath('/home//...//.//'));
-  print(solution.simplifyPath('/h/o/m/../e/..'));
-  print(solution.simplifyPath('/a/../../c/'));
+  print(solution.simplifyPath('/e/./././e/.'));
+  print(solution.simplifyPath('/.'));
+  print(solution.simplifyPath('/../../c/'));
 }
