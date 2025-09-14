5d804b7b4717fc80e0849f9c7295bc2486b14ea8
xunilat <xunilat@gmail.com>
2025-09-14 14:54:46 +0800

    commit 1 number

---
+++ 搜索旋转排序数组II.dart
@@ -0,0 +1,33 @@
+class Solution {
+  bool search(List<int> nums, int target) {
+    int left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) ~/ 2;
+
+      if (nums[mid] == target) return true;
+
+      if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+        left++;
+        right--;
+        continue;
+      }
+
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target < nums[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else {
+        if (nums[mid] < target && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+
+    return false;
+  }
+}
