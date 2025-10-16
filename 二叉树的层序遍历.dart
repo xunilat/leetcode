8f838d2218e2010c5547d2866057ecf88319c2a6
xunilat <xunilat@gmail.com>
2025-10-16 14:37:32 +0800

    topic 102

---
+++ 二叉树的层序遍历.dart
@@ -0,0 +1,31 @@
+import 'dart:collection';
+
+class Solution {
+  List<List<int>> levelOrder(TreeNode? root) {
+    Queue<TreeNode> queue = Queue();
+    List<List<int>> res = [];
+    if (root != null) queue.add(root);
+    while (!queue.isEmpty) {
+      List<int> tmp = [];
+      for (int i = queue.length;i > 0;i--) {
+        TreeNode node = queue.removeFirst();
+        tmp.add(node.val);
+        if (node.left != null) queue.add(node.left!);
+        if (node.right != null) queue.add(node.right!);
+      }
+      res.add(tmp);
+    }
+    return res;
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0,this.left,this.right]);
+}
+
+void main() {
+  print(Solution().levelOrder(TreeNode(3,TreeNode(9,null,TreeNode(10)),TreeNode(20,TreeNode(15),TreeNode(7)))));
+}
